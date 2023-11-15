cafb6c982d131ca61b419bff55606f6c180f6f59
Ali Rezai <Ali.rezai27461@gmail.com>
2023-11-15 15:05:34 +0100

    change

--- src/js/main.js
+++ src/js/main.js
@@ -19,9 +19,9 @@ const samsungUrl = "https://images.ctfassets.net/iidtcybzv1rq/4JMKoFb9bGv1Y7hov0
 const iphoneXUrl = "https://images.ctfassets.net/iidtcybzv1rq/4IrjqxBE6xOUkcESDz4sW4/5980ef1d0e3502510e23585273894dcb/iPhone-15-Pro-Bla.png?fm=jpg&fl=progressive&h=330&q=80";
 const iphone13Url = "https://images.ctfassets.net/iidtcybzv1rq/2ohEy4RiyfhGEyVDujMOkf/0e78d30c3e149a46749972a188bc43bc/iPhone_13_Midnight.png?fm=jpg&fl=progressive&h=330&q=80";
 
-const moble1 = new Mobile("Iphone", "iphone x", 2022 , iphoneXUrl);
+const moble1 = new Mobile("Iphone", "Iphone x", 2022 , iphoneXUrl);
 const moble2 = new Mobile("Samsung", "Galaxy S23 Ultra", 2020, samsungUrl);
-const moble3 = new Mobile("Iphone", "iphone 13", 2021, iphone13Url);
+const moble3 = new Mobile("Iphone", "Iphone 13", 2021, iphone13Url);
 
 const mobiles = [moble1, moble2, moble3]
 
@@ -36,11 +36,13 @@ for (let i = 0; i < mobiles.length; i++) {
   const title = document.createElement("h2");
   const mobileModel = document.createElement("h2");
   const mobileYear = document.createElement("h2");
+
+  mobile.className = "mobile";
   
   mobileImg.src = mobiles[i].img;
-  title.innerHTML = mobiles[i].brand;
-  mobileModel.innerHTML = mobiles[i].model;
-  mobileYear.innerHTML = mobiles[i].year;
+  title.innerHTML = "Märke :"  + mobiles[i].brand;
+  mobileModel.innerHTML = "modell: " +mobiles[i].model;
+  mobileYear.innerHTML = "År: " + mobiles[i].year;
 
 
   mobileContainer.appendChild(mobile);
