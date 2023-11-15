58296f9cd3cb7d877bfd4a8fe5abf84c95fe6201
Timmy <Timmy.viinamaki@gmail.com>
2023-11-15 15:10:51 +0100

    Lista

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import "./../scss/style.scss";
+
+class Personer {
+  namn;
+  ålder;
+  kön;
+
+  constructor(namn, smak, bild) {
+    this.namn = namn;
+    this.ålder = ålder;
+    this.kön = kön;
+  }
+}
+
