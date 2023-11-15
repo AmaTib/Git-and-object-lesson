4addb131cf8fbb16c608ba658321d6064300177d
Timmy <Timmy.viinamaki@gmail.com>
2023-11-15 14:54:08 +0100

    Lista

---
+++ src/js/models/Personer.js
@@ -0,0 +1,11 @@
+export class Personer {
+    name;
+    age;
+    gender;
+  
+    constructor(name, birthyear, gender) {
+      this.name = name;
+      this.birthyear = birthyear;
+      this.gender = gender;
+    }
+  }
